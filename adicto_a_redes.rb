45a5424051f307fc6b06e1c435150c52c8ebc099
Dianapineda20 <53360082+Dianapineda20@users.noreply.github.com>
2020-07-11 19:36:38 -0400

    desafio adicto a redes

---
+++ adicto_a_redes.rb
@@ -0,0 +1,19 @@
+array = [120, 50, 600, 30, 90, 10, 200, 0, 500]
+
+def scan_addicts(array)
+    results = []
+    n = array.count
+    n.times do |i|
+        if array[i] < 90
+            results.push "bien"
+        elsif array[i] >= 90 && array[i] < 180
+            results.push "mejorable"
+        elsif array[i] >= 180
+            results.push "mal"
+        end
+    end
+    print results 
+end
+
+scan_addicts(array)
+puts
