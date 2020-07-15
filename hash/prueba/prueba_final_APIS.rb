d7da13b4ae28f1677342feb0314a6bcf7e6449d5
Dianapineda20 <53360082+Dianapineda20@users.noreply.github.com>
2020-07-15 18:42:35 -0400

    corregi codigo

--- hash/prueba/prueba_final_APIS.rb
+++ hash/prueba/prueba_final_APIS.rb
@@ -28,19 +28,27 @@ def build_web_page(response)
         end
     end
     
-    output = "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
-    fotos.each do |img|
-        output +="\n\t\t\t<li><img src=#{img} /></li>"
-    end
-    output += "\n\t\t</ul>\n\t</body>\n</html>"
-    out_file = File.new("index.html", "w") 
-    out_file.puts (output)
-    out_file.close
-end
+#     output = "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
+#     fotos.each do |img|
+#         output +="\n\t\t\t<li><img src=#{img} /></li>"
+#     end
+#     output += "\n\t\t</ul>\n\t</body>\n</html>"
+#     out_file = File.new("index.html", "w") 
+#     out_file.puts (output)
+#     out_file.close
+# end
 
 hash = request 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10', '1nCzmBKkvH7vo8CKIU236gL3zMOnUKzo4k7qGoL6'
 
 build_web_page hash
 
 
-
+# def photos_count 
+#     fotos = []
+#     response["photos"].each do |item|
+#         item.each do |k, v|
+#             if k == 'img_src'
+#                 fotos.push(v)
+#             end
+#         end
+#     end
